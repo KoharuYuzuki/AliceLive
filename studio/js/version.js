45d718f9f636f7f92837e8c32b666e9df5125523
KoharuYuzuki <79073521+KoharuYuzuki@users.noreply.github.com>
2021-02-17 18:26:01 +0900

    Version display feature implemented

---
+++ studio/js/version.js
@@ -0,0 +1,25 @@
+'use strict';
+
+(() => {
+
+  const title = document.createElement('title');
+  title.innerText = 'Studio | Alice Live!';
+  const url = chrome.runtime.getURL('manifest.json');
+
+  addEventListener('load', () => {
+    document.head.appendChild(title);
+    fetch(url)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      }
+    })
+    .then((json) => {
+      if (json.version) {
+        title.innerText = `${title.innerText} v${json.version}`;
+      }
+    })
+    .catch((e) => console.error(e));
+  }, false);
+
+})();
